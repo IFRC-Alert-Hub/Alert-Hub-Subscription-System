9d094f402cf7e77216808ffbe59cd1f884abdd4a
xiao <zczqxy0@ucl.ac.uk>
2023-06-08 14:34:12 +0100

    update settings.py

--- project/settings.py
+++ project/settings.py
@@ -28,13 +28,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1187ee740a5e11ef86a3df5e50df7d04dd61539de55bcc7facd6faa3c2ee69e3'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
