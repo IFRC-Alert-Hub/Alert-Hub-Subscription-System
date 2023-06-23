cd5f20f7bcffa7524440109a13fb955e835a0a28
Yuechen Chen <yuechen.chen.22@ucl.ac.uk>
2023-06-23 17:50:10 +0100

    Support listSubscriptionByUserId and fix some minor problems based on discussion

--- subscription_dir/admin.py
+++ subscription_dir/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import User, Region, Subscription
+from .models import Subscription
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Region)
 admin.site.register(Subscription)
