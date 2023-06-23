128eb54ed3dda1f0be5296cc618746ba6a23db2e
xiao <zczqxy0@ucl.ac.uk>
2023-06-23 22:04:28 +0100

    updated code style

--- project/urls.py
+++ project/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from sample_dir.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name = 'index'),
-    path('alerts/',include('sample_dir.urls')),
-    path('subscription/', include('subscription_dir.urls'))
+    path('', index, name='index'),
+    path('alerts/', include('sample_dir.urls')),
+    path('subscription/', include('subscription_dir.urls')),
+    path('users/', include('users_dir.urls')),
 ]
