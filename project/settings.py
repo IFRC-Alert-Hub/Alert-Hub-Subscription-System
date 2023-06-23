e60cfa083eca527fb87211195ea47b7be3c41b93
xiao <zczqxy0@ucl.ac.uk>
2023-06-23 18:29:03 +0100

    update settings and production.py

--- project/settings.py
+++ project/settings.py
@@ -10,24 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 
 
-# get current env var
-DJANGO_ENV = os.getenv("DJANGO_ENV", default="dev")
-
-# choose the envfile
-if DJANGO_ENV == "prod":
-    dotenv_file = ".env.prod"
-else:
-    dotenv_file = ".env.dev"
-
-# ensure .env.dev file exists
-dotenv_file = Path(__file__).resolve().parent.parent / dotenv_file
-
-# load .env.dev file
-load_dotenv(dotenv_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default=False) == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
