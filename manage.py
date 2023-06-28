ae11b00fb92c3870c311cdf98b0d8852473de7c7
Xiao057 <135339679+Xiao057@users.noreply.github.com>
2023-06-28 16:23:52 +0100

    Revert "Refactor CICD for Azure development (#12)"
    This reverts commit 5ea637a4ea711536ba8d05e3b3e56ea9afcbf723.

--- manage.py
+++ manage.py
@@ -6,10 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    # When running on Azure App Service you should use the production settings.
-    settings_module = "project.production" if 'WEBSITE_HOSTNAME' in os.environ else 'project.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
