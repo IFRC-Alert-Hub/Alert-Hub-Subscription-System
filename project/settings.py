61f95dab74ad8bff4759089c0d6957714a8875cf
Yuechen Chen <yuechen.chen.22@ucl.ac.uk>
2023-06-29 10:08:52 +0100

    Revert "Revert "Refactor CICD for Azure development (#12)""
    This reverts commit 5102418430c928da9607860f7bb83de14653dd50.

--- project/settings.py
+++ project/settings.py
@@ -10,23 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+from dotenv import load_dotenv
 
-
-# get current env var
-DJANGO_ENV = os.getenv("DJANGO_ENV", default="dev")
-
-# choose the envfile
-if DJANGO_ENV == "prod":
-    dotenv_file = ".env.prod"
-else:
-    dotenv_file = ".env.dev"
-
-# ensure .env.dev file exists
-dotenv_file = Path(__file__).resolve().parent.parent / dotenv_file
-
-# load .env.dev file
+# Load environment variables for .env file
+dotenv_file = Path(__file__).resolve().parent.parent / '.env'
 load_dotenv(dotenv_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default=False) == 'True'
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
