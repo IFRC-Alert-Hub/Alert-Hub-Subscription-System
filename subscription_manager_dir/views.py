8be72bbd89607af79f818bcc139f6d8136acb924
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-01 00:14:18 +0100

    Whenever database gets new alert, CAP aggregator will send this alert to the Alert-Hub backend

--- subscription_manager_dir/views.py
+++ subscription_manager_dir/views.py
@@ -1,32 +1,49 @@
 from django.http import HttpResponse
 from .tasks import send_mail_fun
-
+import json
 def send_mail_to_all(request):
     send_mail_fun.delay()
     return HttpResponse("Sent")
 
-
-import json
-
-from django.shortcuts import render
+import asyncio
 import websocket
-import ssl
-from django.http import HttpResponse
+import threading
 
+#def send_packet(request):
+#    asyncio.run(receive_messages())
+#    return HttpResponse("Good")
+
+
+def run_websocket():
+    def on_message(ws, message):
+        # Process the received message
+        print("Received message:", message)
+
+
+
+    # Create a WebSocket connection
+    ws = websocket.WebSocketApp("ws://127.0.0.1:8000/ws/fetch_new_alert/1a/")
+
+    # Set the callback function for incoming messages
+    ws.on_message = on_message
+
+
+    # Start the WebSocket connection
+    ws.run_forever()
+
+
+
+async def main():
+    # Run the WebSocket client in a separate thread
+    websocket_thread = threading.Thread(target=run_websocket)
+    websocket_thread.start()
+
+    # Run the asyncio event loop in the main thread
+    while True:
+        # Perform other asyncio-related tasks here if needed
+        await asyncio.sleep(1)
 # Create your views here.
 # Disable SSL certificate verification if needed
 def send_packet(request):
-    websocket.enableTrace(True)  # Optional: Enable trace for debugging
-    ws = websocket.create_connection(
-        "ws://127.0.0.1:8000/ws/fetch_new_alert/w5/"
-    )
-
-    packet = "Your packet data"  # Replace with your actual packet data
-    ws.send(json.dumps(packet))
-
-    result = ws.recv()
-    print("Received response from server:", result)
-
-    ws.close()
-
-    return HttpResponse(f"Received response from server: {result}")
+    # Run the asyncio event loop
+    asyncio.run(main())
