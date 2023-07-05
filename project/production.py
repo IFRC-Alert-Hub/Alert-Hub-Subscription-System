35922ba087f22a6230fc387ffcb0aae34d6e3dd6
xiao <zczqxy0@ucl.ac.uk>
2023-07-05 11:39:53 +0100

    changed production.py

--- project/production.py
+++ project/production.py
@@ -48,3 +48,10 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'django-cache'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'verify_code',
+    }
+}
