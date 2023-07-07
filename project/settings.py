ef959dac308b0e770ef2e054bd1cbd1da803e504
Xiao057 <135339679+Xiao057@users.noreply.github.com>
2023-07-07 12:20:04 +0100

    try to fix csrf bugs (#19)
    * updated deploy yml
    
    * diabled scrf
    
    * applied csrf_exempt for graphql schema
    
    * added csrf trusted origins

--- project/settings.py
+++ project/settings.py
@@ -30,8 +30,8 @@ DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = [
+    'https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # Application definition
 
