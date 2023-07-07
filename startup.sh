b8d17dfb5bd1aa00ca00fd25c26ab567ffa4cb05
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-07 12:00:44 +0100

    Try Deploy the Current Backend

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
 python manage.py migrate
-celery -A project worker -l info --pool=solo & celery -A project beat -l info
+gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
+    '-' --error-logfile '-' --bind=0.0.0.0:8000 \
+     --chdir=/home/site/wwwroot capaggregator.wsgi & celery -A project worker -l info --pool=solo & celery -A project beat -l info
