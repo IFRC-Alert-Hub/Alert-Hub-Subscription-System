67dd221af67ff549b69c507d3b0a60e2f00519e7
xiao <zczqxy0@ucl.ac.uk>
2023-07-07 01:03:37 +0100

    update error message type

--- user_dir/schema.py
+++ user_dir/schema.py
@@ -1,3 +1,4 @@
+import json
 import random
 from datetime import timedelta
 
@@ -14,6 +15,14 @@ from django.core.cache import cache
 from .models import CustomUser
 from .tasks import send_email
 from .utils import generate_jti
+
+
+class ErrorType(graphene.ObjectType):
+    verifyCode = graphene.String()
+    email = graphene.String()
+    session = graphene.String()
+    userName = graphene.String()
+    user = graphene.String()
 
 
 class UserType(DjangoObjectType):
@@ -45,7 +54,7 @@ class Register(graphene.Mutation):
     """ Mutation to register a user """
 
     success = graphene.Boolean()
-    errors = graphene.List(graphene.String)
+    errors = graphene.Field(ErrorType)
 
     class Arguments:
         email = graphene.String(required=True)
@@ -54,17 +63,17 @@ class Register(graphene.Mutation):
 
     def mutate(self, info, email, password, verify_code):
         if CustomUser.objects.filter(email__iexact=email).exists():
-            errors = ['emailAlreadyExists']
+            errors = ErrorType(email='Email already exists.')
             return Register(success=False, errors=errors)
 
         true_code = cache.get(email + "_register")
 
         if true_code is None:
-            errors = ['verifyCodeExpired']
+            errors = ErrorType(verifyCode='Verify code has expired.')
             return Register(success=False, errors=errors)
 
         if verify_code != true_code:
-            errors = ['wrongVerifyCode']
+            errors = ErrorType(verifyCode='Wrong verify code.')
             return Register(success=False, errors=errors)
 
         # create user
@@ -82,14 +91,14 @@ class SendVerifyEmail(graphene.Mutation):
     """ Mutation for sending email verification for first registration"""
 
     success = graphene.Boolean()
-    errors = graphene.List(graphene.String)
+    errors = graphene.Field(ErrorType)
 
     class Arguments:
         email = graphene.String(required=True)
 
     def mutate(self, info, email):
         if CustomUser.objects.filter(email__iexact=email).exists():
-            errors = ['emailAlreadyExists']
+            errors = ErrorType(email='Email already exists.')
             return SendVerifyEmail(success=False, errors=errors)
 
         email_verification_token = str(random.randint(0, 999999))
@@ -122,14 +131,14 @@ class ResetEmail(graphene.Mutation):
     """ Mutation to request to reset an email """
 
     success = graphene.Boolean()
-    errors = graphene.List(graphene.String)
+    errors = graphene.Field(ErrorType)
 
     @login_required
     def mutate(self, info):
         if info.context.user.is_authenticated:
             user = info.context.user
         else:
-            errors = ['wrongUser']
+            errors = ErrorType(user='wrong User')
             return ResetEmail(success=False, errors=errors)
 
         verification_code = str(random.randint(0, 999999))
@@ -151,7 +160,7 @@ class ResetEmailConfirm(graphene.Mutation):
     """ Mutation for confirm requesting a email reset """
 
     success = graphene.Boolean()
-    errors = graphene.List(graphene.String)
+    errors = graphene.Field(ErrorType)
     token = graphene.String()
 
     class Arguments:
@@ -162,17 +171,17 @@ class ResetEmailConfirm(graphene.Mutation):
         if info.context.user.is_authenticated:
             user = info.context.user
         else:
-            errors = ['wrongUser']
+            errors = ErrorType(user='wrong User')
             return ResetEmailConfirm(success=False, errors=errors)
 
         true_code = cache.get(user.email + "_email_reset")
 
         if true_code is None:
-            errors = ['verifyCodeExpired']
+            errors = ErrorType(verifyCode='Verify code has expired.')
             return ResetEmailConfirm(success=False, errors=errors)
 
         if verify_code != true_code:
-            errors = ['wrongVerifyCode']
+            errors = ErrorType(verifyCode='Wrong verify code.')
             return ResetEmailConfirm(success=False, errors=errors)
 
         reset_token = uuid4()
@@ -187,7 +196,7 @@ class SendNewVerifyEmail(graphene.Mutation):
     """ Mutation for requesting a password reset email """
 
     success = graphene.Boolean()
-    errors = graphene.List(graphene.String)
+    errors = graphene.Field(ErrorType)
 
     class Arguments:
         token = graphene.UUID(required=True)
@@ -199,21 +208,21 @@ class SendNewVerifyEmail(graphene.Mutation):
         if info.context.user.is_authenticated:
             user = info.context.user
         else:
-            errors = ['wrongUser']
+            errors = ErrorType(user='wrong User')
             return ResetEmailConfirm(success=False, errors=errors)
 
         true_code = cache.get(user.email + "_confirm")
 
         if true_code is None:
-            errors = ['sessionExpired']
+            errors = ErrorType(session='This session has expired')
             return SendNewVerifyEmail(success=False, errors=errors)
 
         if CustomUser.objects.filter(email__iexact=new_email).exists():
-            errors = ['emailAlreadyExists']
+            errors = ErrorType(email='Email already exists.')
             return SendNewVerifyEmail(success=False, errors=errors)
 
         if token != true_code:
-            errors = ['wrongToken']
+            errors = ErrorType(verifyCode='Wrong verify code.')
             return SendNewVerifyEmail(success=False, errors=errors)
 
         email_verification_token = str(random.randint(0, 999999))
@@ -225,7 +234,6 @@ class SendNewVerifyEmail(graphene.Mutation):
 
         send_email.delay(new_email, 'Verify your new email.', 'email_verification.html', context)
 
-
         return SendNewVerifyEmail(success=True, token=token)
 
 
@@ -233,7 +241,7 @@ class NewEmailConfirm(graphene.Mutation):
     """ Mutation to confirm the new email. """
 
     success = graphene.Boolean()
-    errors = graphene.List(graphene.String)
+    errors = graphene.Field(ErrorType)
 
     class Arguments:
         new_email = graphene.String(required=True)
@@ -244,17 +252,17 @@ class NewEmailConfirm(graphene.Mutation):
         if info.context.user.is_authenticated:
             user = info.context.user
         else:
-            errors = ['wrongUser']
+            errors = ErrorType(user='wrong User')
             return ResetEmailConfirm(success=False, errors=errors)
 
         true_code = cache.get(new_email + "_new_verify")
 
         if true_code is None:
-            errors = ['verifyCodeExpired']
+            errors = ErrorType(verifyCode='Verify code has expired.')
             return NewEmailConfirm(success=False, errors=errors)
 
         if verify_code != true_code:
-            errors = ['wrongVerifyCode']
+            errors = ErrorType(verifyCode='Wrong verify code.')
             return NewEmailConfirm(success=False, errors=errors)
 
         # create user
@@ -271,7 +279,7 @@ class UpdateProfile(graphene.Mutation):
     """ Mutation to update a user's profile information """
 
     success = graphene.Boolean()
-    errors = graphene.List(graphene.String)
+    errors = graphene.Field(ErrorType)
 
     class Arguments:
         username = graphene.String(default_value=None)
@@ -285,7 +293,7 @@ class UpdateProfile(graphene.Mutation):
 
         if CustomUser.objects.filter(username__iexact=username).exclude(
                 pk=user.pk).exists() and username:
-            errors = ['usernameAlreadyExists']
+            errors = ErrorType(userName='usernameAlreadyExists')
             return UpdateProfile(success=False, errors=errors)
 
         user.username = username
@@ -299,14 +307,14 @@ class UpdateProfile(graphene.Mutation):
 class Logout(graphene.Mutation):
     id = graphene.ID()
     success = graphene.Boolean()
-    errors = graphene.List(graphene.String)
+    errors = graphene.Field(ErrorType)
 
     @classmethod
     def mutate(cls, root, info, **kwargs):
         try:
             user = info.context.user
             if not user.is_authenticated:
-                errors = ["User not authenticated."]
+                errors = ErrorType(user="User not authenticated.")
                 return cls(success=False, errors=errors)
 
             user.jti = generate_jti()
@@ -321,7 +329,7 @@ class ResetPassword(graphene.Mutation):
     """ Mutation for requesting a password reset email """
 
     success = graphene.Boolean()
-    errors = graphene.List(graphene.String)
+    errors = graphene.Field(ErrorType)
 
     class Arguments:
         email = graphene.String(required=True)
@@ -330,7 +338,7 @@ class ResetPassword(graphene.Mutation):
         try:
             user = CustomUser.objects.get(email=email)
         except CustomUser.DoesNotExist:
-            errors = ['emailDoesNotExists']
+            errors = ErrorType(email='Email does not exists.')
             return ResetPassword(success=False, errors=errors)
 
         # create email verification link
@@ -352,22 +360,28 @@ class ResetPasswordConfirm(graphene.Mutation):
     """ Mutation for requesting a password reset email """
 
     success = graphene.Boolean()
-    errors = graphene.List(graphene.String)
+    errors = graphene.Field(ErrorType)
 
     class Arguments:
-        token = graphene.String(required=True)
+        verifyCode = graphene.String(required=True)
         password = graphene.String(required=True)
 
-    def mutate(self, info, token, password):
+    def mutate(self, info, verifyCode, password):
         # Check if the token is empty
-        if not token:
-            errors = ['emptyVerifyCode']
+        if not verifyCode:
+            errors = ErrorType(verifyCode='Empty verify code')
             return ResetPasswordConfirm(success=False, errors=errors)
 
         try:
-            user = CustomUser.objects.get(password_reset_token=token)
+            user = CustomUser.objects.get(password_reset_token=verifyCode)
         except CustomUser.DoesNotExist:
-            errors = ['wrongVerifyCode']
+            errors = ErrorType(verifyCode='Wrong verify code.')
+            return ResetPasswordConfirm(success=False, errors=errors)
+
+        now = timezone.now()
+
+        if user.password_reset_token_expires_at > now:
+            errors = ErrorType(verifyCode='Verify code has expired.')
             return ResetPasswordConfirm(success=False, errors=errors)
 
         user.set_password(password)
