db63a32535bbaeb75c54cbb269f9dc793d48bba9
Xiao057 <135339679+Xiao057@users.noreply.github.com>
2023-07-09 12:52:00 +0100

    Update settings.py
    just for testing

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default=False)
+# DEBUG = os.getenv('DEBUG', default=False)
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
