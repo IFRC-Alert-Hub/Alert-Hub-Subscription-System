d317e00439da8f7a75603ab7ca3e4f4ab2fe23b9
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-11 13:11:40 +0100

    Closing Websocket After The Server is Closed

--- subscription_manager_dir/establish_websockets_connection.py
+++ subscription_manager_dir/establish_websockets_connection.py
@@ -14,49 +14,50 @@ logger = logging.getLogger('websockets')
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
 
-
-@sync_to_async
-def filter_subscription(alert_map):
-    return list(Subscription.objects.filter(
-        country_ids__contains=[alert_map["country_id"]],
-        urgency_array__contains=[alert_map["urgency"]],
-        severity_array__contains=[alert_map["severity"]],
-        certainty_array__contains=[alert_map["certainty"]]
-    ))
-
-
-async def process_incoming_alert(message):
-    alert_map = json.loads(message)["message"]
-    print(alert_map)
-    matched_subscriptions = await filter_subscription(alert_map)
+class WebsocketConnection:
+    def __init__(self):
+        self.websocket = None
+    @sync_to_async
+    def filter_subscription(alert_map):
+        return list(Subscription.objects.filter(
+            country_ids__contains=[alert_map["country_id"]],
+            urgency_array__contains=[alert_map["urgency"]],
+            severity_array__contains=[alert_map["severity"]],
+            certainty_array__contains=[alert_map["certainty"]]
+        ))
 
 
-    for subscription in matched_subscriptions:
-        context = {
-            'country_name': alert_map["country_name"],
-            'urgency': alert_map["urgency"],
-            'severity': alert_map["severity"],
-            'certainty': alert_map["certainty"],
-        }
-        print("b")
-        try:
-            send_subscription_email.delay(subscription.user_id,
+    async def process_incoming_alert(self, message):
+        alert_map = json.loads(message)["message"]
+        print(alert_map)
+        matched_subscriptions = await self.filter_subscription(alert_map)
+
+
+        for subscription in matched_subscriptions:
+            context = {
+                'country_name': alert_map["country_name"],
+                'urgency': alert_map["urgency"],
+                'severity': alert_map["severity"],
+                'certainty': alert_map["certainty"],
+            }
+            try:
+                send_subscription_email.delay(subscription.user_id,
                                           'New Alerts Matching Your Subscription',
                                           'subscription_email.html', context)
-        except Exception as general_exception:  # pylint: disable=broad-except
-            print(f"Error: {general_exception}")
+            except Exception as general_exception:  # pylint: disable=broad-except
+                print(f"Error: {general_exception}")
 
 
-async def establish_websocket_connect():
-    host_name = os.environ.get("CAPAGGREGATOR_CONNECTION_WEBSITE")
-    # Connect to the WebSocket server
-    async with websockets.connect(f'wss://{host_name}/ws/fetch_new_alert/1a/',
+    async def establish_websocket_connect(self):
+        host_name = os.environ.get("CAPAGGREGATOR_CONNECTION_WEBSITE")
+        # Connect to the WebSocket server
+        async with websockets.connect(f'ws://{host_name}/ws/fetch_new_alert/1a/',
                                   origin=os.environ.get("WEBSOCKET_ORIGIN")) as websocket:
-        while True:
-            try:
-                message = await websocket.recv()
-                await process_incoming_alert(message=message)
-            except Exception as e:
-                print("Websocket Closed")
-                await websocket.close()
-                break
+            self.websocket = websocket
+            while True:
+                    message = await websocket.recv()
+                    await self.process_incoming_alert(message=message)
+
+    async def close_connection(self):
+        if self.websocket:
+            await asyncio.ensure_future(self.websocket.close())
--- subscription_manager_dir/management/commands/startconnection.py
+++ subscription_manager_dir/management/commands/startconnection.py
@@ -5,19 +5,25 @@ from django.core.management.base import BaseCommand, CommandError
 import os
 from django.http import HttpResponse
 
-from subscription_manager_dir.establish_websockets_connection import establish_websocket_connect
+from subscription_manager_dir import establish_websockets_connection
+
+
+
+
 class Command(BaseCommand):
     help = "Starting connections to " + os.environ.get("CAPAGGREGATOR_CONNECTION_WEBSITE") + "."
 
     def handle(self, *args, **options):
         while True:
+            WebsocketConnection = establish_websockets_connection.WebsocketConnection()
+            # Run the WebSocket connection
             try:
-                # Run the WebSocket connection
-                asyncio.get_event_loop().run_until_complete(establish_websocket_connect())
+                asyncio.get_event_loop().run_until_complete(
+                    WebsocketConnection.establish_websocket_connect())
                 break
-            except Exception as general_exception:  # pylint: disable=broad-except
-                print(general_exception)
-                return 'Failed'
+            except KeyboardInterrupt as general_exception:  # pylint: disable=broad-except
+                WebsocketConnection.websocket.close()
+                return "The connection is closed!"
 
     def handle_interrupt(self, signum, frame):
         # Clean up tasks (if needed)
