6e845c87011e6e702f962c17c7786cafcde8b265
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-07-16 01:11:32 +0100

    Fixing problem of multi wsgi worker automate the connection process

--- startup.sh
+++ startup.sh
@@ -2,4 +2,3 @@ gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
      --chdir=/home/site/wwwroot project.wsgi & celery -A project worker -l info --pool=solo
 python manage.py migrate
-python manage.py collectstatic
