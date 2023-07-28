04682a951023c3564001957c27c0dbc99724099d
xiao <zczqxy0@ucl.ac.uk>
2023-07-28 02:11:18 +0100

    send email every 12 hours

--- filter/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Alert,AlertInfo,Country, Region
-admin.site.register(Alert)
-admin.site.register(AlertInfo)
-admin.site.register(Region)
-admin.site.register(Country)
