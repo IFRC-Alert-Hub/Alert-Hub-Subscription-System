49089c1ed94b2b848fb7c2f1693145c1b96a3b54
Yuechen Chen <yuechen.chen.22@ucl.ac.uk>
2023-07-30 11:28:08 +0800

    Make admin transformation generator

--- polygon_dir/polygon_csv_generator/admin1_generator.py
+++ polygon_dir/polygon_csv_generator/admin1_generator.py
@@ -1,13 +1,37 @@
-import geopandas as gpd
+import json
+import csv
 
-## Transform data to a csv file
-# id: districtName (GID_1)
-# admin1Name: name of that administrative region (NAME_1)
-# country: foreign key (country iso3)
-# polygon: polygon string
+# ## Transform data to a csv file
+# # id: districtName (GID_1)
+# # name: name of administrative level (NAME_1)
+# # country: foreign key (country iso3)
+# # polygon: polygon string
 
-file_path = '/Users/chenyuechen/Documents/TestData/world/world.geojson'
-# file_path = '/Users/chenyuechen/Documents/TestData/countries/AFG/afg.geojson'
-gdf = gpd.read_file(file_path)
+input_file_path = '/Users/chenyuechen/Documents/TestData/world/world.geojson'
+# input_file_path = "/Users/chenyuechen/Documents/TestData/countries/AFG/afg.geojson"
 
-print("hello world")
+# Load JSON data from the file
+with open(input_file_path, "r") as input_file:
+    data = json.load(input_file)
+
+admin1_data = []
+for item in data['features']:
+    admin1_item = dict()
+    admin1_item['id'] = item['properties']['GID_1']
+    admin1_item['name'] = item['properties']['NAME_1']
+    admin1_item['country'] = item['properties']['GID_0']
+    admin1_item['polygon'] = item['geometry']['coordinates']
+    admin1_data.append(admin1_item)
+
+output_file_path = '/Users/chenyuechen/Documents/TestData/world/world.csv'
+with open(output_file_path, "x", newline="") as output_file:
+    fieldnames = ["id", "name", "country", "polygon"]
+    writer = csv.DictWriter(output_file,
+                            fieldnames=fieldnames)
+
+    # Write the header (field names)
+    writer.writeheader()
+
+    # Write the data rows
+    for row in admin1_data:
+        writer.writerow(row)
