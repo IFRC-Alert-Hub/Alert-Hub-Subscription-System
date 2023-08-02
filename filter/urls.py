5ea0405c1c29275c2870bf470f308c5ce1bbbbfe
xiao <zczqxy0@ucl.ac.uk>
2023-08-02 13:46:00 +0100

    deleted filter

--- filter/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from graphene_django.views import GraphQLView
-from sample_dir.schema import schema
-
-urlpatterns = [
-    path('graphql', GraphQLView.as_view(graphiql = True, schema = schema)),
-]
