9a9965ead1aa2296c3b772e1590dd0bed8a88adb
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-08-08 17:26:39 +0100

    View subscription alerts

--- subscription_manager_dir/apps.py
+++ subscription_manager_dir/apps.py
@@ -9,7 +9,7 @@ import time
 from django.apps import AppConfig
 from django.db.models.signals import post_save
 from django.core.cache import cache
-from .cache import cache_subscription_alert
+
 
 
 class SubscriptionManagerConfig(AppConfig):
@@ -20,12 +20,9 @@ class SubscriptionManagerConfig(AppConfig):
         if 'WEBSITE_HOSTNAME' in os.environ or \
             ('WEBSITE_HOSTNAME' not in os.environ and 'runserver' in sys.argv):
             from .subscription_alert_mapping import map_alerts_to_subscription
+            from .cache import cache_subscriptions_alert
             result = cache.add('locked', True, timeout=5)
             if result:
-                SubscriptionAlerts = self.get_model(
-                    "SubscriptionAlerts")
-                post_save.connect(cache_subscription_alerts, sender=SubscriptionAlerts)
                 map_alerts_to_subscription()
+                cache_subscriptions_alert()
 
-def cache_subscription_alerts(sender, instance, *args, **kwargs):
-    cache_subscription_alert(instance)
--- subscription_manager_dir/cache.py
+++ subscription_manager_dir/cache.py
@@ -1,10 +1,13 @@
 import json
 from django.core.cache import cache
-
+from subscription_dir.models import Subscription
+def cache_subscriptions_alert():
+    subscriptions = Subscription.objects.all()
+    for subscription in subscriptions:
+        cache_subscription_alert(subscription)
 def cache_subscription_alert(subscription):
     subscription_alerts_dict = subscription.subscription_alerts_to_dict()
-    print("id is:" + str(subscription.id))
-    cache.set("subscription" + str(subscription.subscription.id), json.dumps(
+    cache.set("subscription" + str(subscription.id), json.dumps(
         subscription_alerts_dict, indent=None), timeout=None)
 
 
