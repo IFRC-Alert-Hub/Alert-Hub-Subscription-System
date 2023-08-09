a327e10db8096159215460d0ab5a3f1bf9fded36
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-08-09 20:31:21 +0100

    Try locating issues

--- subscription_manager_dir/apps.py
+++ subscription_manager_dir/apps.py
@@ -17,12 +17,13 @@ class SubscriptionManagerConfig(AppConfig):
     name = 'subscription_manager_dir'
 
     def ready(self):
-        if ('WEBSITE_HOSTNAME' in os.environ and 'migrate' not in sys.argv and 'collectstatic' not in sys.argv) \
-                or \
-            ('WEBSITE_HOSTNAME' not in os.environ and 'runserver' in sys.argv):
-            result = cache.add('locked', True, timeout=60)
-            if result:
-                print("????")
+        #if ('WEBSITE_HOSTNAME' in os.environ and 'migrate' not in sys.argv and 'collectstatic'
+        # not in sys.argv) \
+        #        or \
+        #     ('WEBSITE_HOSTNAME' not in os.environ and 'runserver' in sys.argv):
+        #    result = cache.add('locked', True, timeout=60)
+        #    if result:
+        print("????")
                 #from .subscription_alert_mapping import map_alerts_to_subscription
                 #from .cache import cache_subscriptions_alert
                 #map_alerts_to_subscription()
