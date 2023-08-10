222b2bd3add1d857d352a28e2ae183e55992544f
cyc <chenyuechen123@126.com>
2023-08-10 17:15:39 +0100

    Rename district to admin1 (#64)
    Co-authored-by: Yuechen Chen <yuechen.chen.22@ucl.ac.uk>

--- subscription_dir/models.py
+++ subscription_dir/models.py
@@ -7,7 +7,7 @@ class Subscription(models.Model):
     subscription_name = models.CharField(default="", verbose_name="subscription_name")
     user_id = models.IntegerField(default=0, verbose_name="user_id")
     country_ids = ArrayField(models.IntegerField(verbose_name='country_ids'), default=list)
-    district_ids = ArrayField(models.IntegerField(verbose_name='district_ids'), default=list)
+    admin1_ids = ArrayField(models.IntegerField(verbose_name='admin1_ids'), default=list)
     urgency_array = ArrayField(models.CharField(verbose_name='urgency_array'), default=list)
     severity_array = ArrayField(models.CharField(verbose_name='severity_array'), default=list)
     certainty_array = ArrayField(models.CharField(verbose_name='certainty_array'), default=list)
