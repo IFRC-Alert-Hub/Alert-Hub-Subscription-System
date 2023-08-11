2d3568149c13487e7694c9b38f3409fc9c9723a3
Yuechen Chen <yuechen.chen.22@ucl.ac.uk>
2023-08-11 21:18:10 +0100

    Add nativegeocoder demos for future geocoder resolver

---
+++ polygon_dir/nativegeocoder/android/src/main/java/ee/forgr/capacitor_nativegeocoder/NativeGeocoderPlugin.java
@@ -0,0 +1,39 @@
+package ee.forgr.capacitor_nativegeocoder;
+
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "NativeGeocoder")
+public class NativeGeocoderPlugin extends Plugin {
+
+  private NativeGeocoder implementation = new NativeGeocoder();
+
+  @Override
+  public void load() {
+    super.load();
+    implementation.context = this.getContext();
+  }
+
+  @PluginMethod
+  public void reverseGeocode(PluginCall call) {
+    Double latitude = call.getDouble("latitude");
+    Double longitude = call.getDouble("longitude");
+    if (latitude == null || longitude == null) {
+      call.reject("Missing latitude or longitude");
+      return;
+    }
+    implementation.reverseGeocode(latitude, longitude, call);
+  }
+
+  @PluginMethod
+  public void forwardGeocode(PluginCall call) {
+    String addressString = call.getString("addressString");
+    if (addressString == null) {
+      call.reject("Missing addressString");
+      return;
+    }
+    implementation.forwardGeocode(addressString, call);
+  }
+}
