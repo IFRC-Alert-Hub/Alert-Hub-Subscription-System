230ae08d85b6eb748f30bd8e3baad4751e6c8bf2
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-08-14 23:20:48 +0100

    append test specification

---
+++ subscription_manager_dir/tests.py
@@ -0,0 +1,19 @@
+#Test Specification
+#unit test
+#Test comparation between alert and subscription
+
+
+#Test add subscription and check many subscription - to - many alerts
+#Test change subscription and check many subscription - to - many alerts
+#Test delete subscription and check many subscription - to - many field
+
+#Test add subscription and test cache
+#Test change subscription and test cache
+#Test delete subscription and test cache
+
+#Test add alert and test many subscription - to - many alerts
+#Test add alert and test cache delete alert and test many subscriptions to many alerts
+#Test delete alert and test cache
+
+#Test when alert is not mapped
+#Test alert to be deleted is not existed test
