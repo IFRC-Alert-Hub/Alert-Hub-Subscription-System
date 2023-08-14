a32dc65b810a97508c80eb579a89c73104175e0c
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-08-14 23:15:54 +0100

    when subscription is not found, return 404 error codes

--- subscription_manager_dir/views.py
+++ subscription_manager_dir/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from .cache import get_subscription_alerts
 
 def get_subscirption_alerts(request, subscription_id):
+    result = get_subscription_alerts(subscription_id)
+    if not result:
+        return HttpResponse("Subscription is not found!", status=404)
     return HttpResponse(get_subscription_alerts(subscription_id))
