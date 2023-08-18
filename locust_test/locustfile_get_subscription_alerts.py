58a28bf9992b6eed0bfec89fb3cdb36db269777f
xiao <zczqxy0@ucl.ac.uk>
2023-08-18 10:00:24 +0100

    add locust files

--- locust_test/locustfile_get_subscription_alerts.py
+++ locust_test/locustfile_get_subscription_alerts.py
@@ -1,14 +1,26 @@
-from locust import HttpUser, task
+import os
+import json
+from locust import HttpUser, task, between
 import csv
 
-class SubscriptionUser(HttpUser):
-    @task
-    def read_csv_and_send_requests(self):
-        with open('fake_users.csv', 'r') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                subscription_ids = row['subscription_ids'].split(',')
-                for subscription_id in subscription_ids:
-                    url = f"https://backend-deploy.azurewebsites.net/subscription_manager/get_subscription_alerts/{subscription_id}/"
-                    self.client.get(url)
+# class SubscriptionUser(HttpUser):
+#     host = "http://127.0.0.1:8000"
+#     wait_time = between(0, 1)
+#
+#     @task
+#     def read_csv_and_send_requests(self):
+#         with open(os.path.join(os.path.dirname(__file__), 'fake_users.csv'), 'r') as csvfile:
+#             reader = csv.DictReader(csvfile)
+#             for row in reader:
+#                 subscription_ids = json.loads(row['subscription_ids'])
+#                 for subscription_id in subscription_ids:
+#                     url = f"/subscription_manager/get_subscription_alerts/{subscription_id}/"
+#                     self.client.get(url)
 
+class SubscriptionUser(HttpUser):
+    wait_time = between(0, 1)
+
+    @task
+    def send_requests(self):
+        url = f"/subscription_manager/get_subscription_alerts/1/"
+        self.client.get(url)
