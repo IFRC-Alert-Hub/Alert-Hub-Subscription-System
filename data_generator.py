bf9fc4cc9b34bdee548a9f6d2cf388407dcc2143
xiao <zczqxy0@ucl.ac.uk>
2023-08-21 01:37:41 +0100

    updated data generator

--- data_generator.py
+++ data_generator.py
@@ -1,22 +1,16 @@
 import csv
 import os
-
 import django
-from django.db import IntegrityError
+from django.core.management import call_command
 
-if 'WEBSITE_HOSTNAME' in os.environ:
-    settings_module = "project.production"
-else:
-    settings_module = "project.settings"
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-
-# Configure Django settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'project.production' if 'WEBSITE_HOSTNAME' in os.environ else 'project.settings')
 django.setup()
 
 from faker import Faker
 import random
 
-from django.core.management import call_command, execute_from_command_line
+from django.db import IntegrityError, transaction
 from django.contrib.auth import get_user_model
 from subscription_dir.models import Subscription
 from subscription_manager_dir.external_alert_models import CapFeedCountry, CapFeedAdmin1
@@ -26,16 +20,14 @@ User = get_user_model()
 
 
 def create_user(email, password):
-    user = User.objects.create(
-        email=email,
-    )
+    user = User.objects.create(email=email)
     user.set_password(password)
     user.save()
     return user
 
 
 def create_subscription(user, country, admin1s):
-    return Subscription.objects.create(
+    return Subscription(
         subscription_name=fake.sentence()[:-1],
         user_id=user.id,
         country_ids=country,
@@ -44,7 +36,7 @@ def create_subscription(user, country, admin1s):
         severity_array=["Severe"],
         certainty_array=['Observed', 'Likely', 'Possible'],
         subscribe_by=["Email"],
-        sent_flag=0,
+        sent_flag=0
     )
 
 
@@ -58,49 +50,49 @@ def generate_fake_users_and_subscriptions(count, csv_filename, country_count, su
 
         countries = list(CapFeedCountry.objects.filter(id=161))
 
-        for i in range(count):
-            print("Creating users: {}/{}".format(i + 1, count))
-            while True:
-                try:
-                    email = fake.email()
-                    password = fake.password()
+        with transaction.atomic():
+            for i in range(count):
+                print("Creating users: {}/{}".format(i + 1, count))
+                while True:
+                    try:
+                        email = fake.unique.email()
+                        password = fake.password()
 
-                    user = create_user(email, password)
-                    users.append(user)
+                        user = create_user(email, password)
+                        users.append(user)
 
-                    subscriptions = []
-                    subscription_ids = ""
-                    # selected_countries = random.sample(countries, country_count)
+                        subscriptions_to_create = []
+                        subscription_ids = ""
 
-                    selected_countries = countries
+                        selected_countries = countries
 
-                    for _ in range(subscription_count):
+                        for _ in range(subscription_count):
+                            for country in selected_countries:
+                                admin1s = CapFeedAdmin1.objects.filter(country=country)
+                                if admin1s:
+                                    selected_admin1s = random.sample(list(admin1s),
+                                                                     random.randint(1,
+                                                                                    len(admin1s)))
+                                    country_ids = [country.id]
+                                    admin1_ids = [admin1.id for admin1 in selected_admin1s]
 
-                        for country in selected_countries:
-                            admin1s = CapFeedAdmin1.objects.filter(country=country)
+                                    subscription = create_subscription(user, country_ids,
+                                                                       admin1_ids)
+                                    subscriptions_to_create.append(subscription)
+                                    subscription_ids = ', '.join(
+                                        [str(subscription.id) for subscription in
+                                         subscriptions_to_create])
 
-                            if admin1s:  # Check if admin1s is not empty
-                                selected_admin1s = random.sample(list(admin1s), random.randint(1, len(admin1s)))
-
-                                country_ids = [country.id]
-                                admin1_ids = [admin1.id for admin1 in selected_admin1s]
-
-                                subscription = create_subscription(user, country_ids, admin1_ids)
-                                subscriptions.append(str(subscription.id))
-
-                                subscription_ids = ', '.join(subscriptions)
-
-                    writer.writerow(
-                            {'email': email, 'password': password, 'subscription_ids': subscription_ids})
-                    break
-                except IntegrityError as e:
-                    # Handle the IntegrityError (duplicate email) exception
-                    print(f"IntegrityError: {e}. Retrying with a different email.")
-                    continue  # Skip this iteration and try again with a different email
+                        Subscription.objects.bulk_create(subscriptions_to_create)
+                        writer.writerow({'email': email, 'password': password,
+                                         'subscription_ids': subscription_ids})
+                        break
+                    except IntegrityError as e:
+                        print(f"IntegrityError: {e}. Retrying with a different email.")
+                        continue
     return users
 
 
 call_command('flush', '--noinput')
 
-# Generate 10 fake users
 generate_fake_users_and_subscriptions(1000, 'fake_users.csv', 1, 10)
