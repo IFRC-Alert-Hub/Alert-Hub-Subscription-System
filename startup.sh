77d27084b7f828094101401d011e54211f7c7d55
Xiao057 <135339679+Xiao057@users.noreply.github.com>
2023-08-22 22:49:35 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@ python manage.py migrate
 python manage.py collectstatic --no-input
 gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
-     --chdir=/home/site/wwwroot project.wsgi & celery -A project worker -l info -c 16 &
+     --chdir=/home/site/wwwroot project.wsgi & celery -A project worker -l info -c 4 &
      celery -A project beat -l info
