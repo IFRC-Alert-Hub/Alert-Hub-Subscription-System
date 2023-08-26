1eadc12eee33e081657e8f70668b8e7816f9f4e0
Xiao057 <135339679+Xiao057@users.noreply.github.com>
2023-08-26 22:29:17 +0100

    Update settings.py
    change smtp account

--- project/settings.py
+++ project/settings.py
@@ -211,8 +211,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'alerthubsystem@gmail.com'
-EMAIL_HOST_PASSWORD = 'wpyffoymvnwfrkox'
+EMAIL_HOST_USER = 'uclalert0@gmail.com'
+EMAIL_HOST_PASSWORD = 'lqlauyiauhrtvoif'
 DEFAULT_FROM_EMAIL = 'IFRC Alert Hub'
 
 CACHES = {
