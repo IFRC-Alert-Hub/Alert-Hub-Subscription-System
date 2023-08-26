f847ad03f530e81e3eb75beed3eccd6a565ba0d2
HangZhou01 <ucabhz9@ucl.ac.uk>
2023-08-26 22:57:53 +0100

    update status code

--- subscription_manager_dir/views.py
+++ subscription_manager_dir/views.py
@@ -10,18 +10,19 @@ def get_subscirption_alerts(request, subscription_id):
         # still mapping the alerts.
         lock_acquired = cache.keys("*")
         if str(subscription_id) in lock_acquired:
-            return HttpResponse("Subscription is still matching alerts!", status=404)
+            return HttpResponse("Subscription is still matching alerts!", status=202)
 
         subscription = Subscription.objects.get(id=subscription_id)
     except Subscription.DoesNotExist:
         return HttpResponse("Subscription is not found!", status=404)
 
     alert_list = subscription.get_alert_id_list()
-    if len(alert_list) == 0:
-        return HttpResponse("There is no alerts!", status=404)
+
+    #if len(alert_list) == 0:
+    #    return HttpResponse("[]", status=200)
 
     result = json.dumps(alert_list)
-    return HttpResponse(result)
+    return HttpResponse(result,status=200)
 
 
 def get_subscription_alerts_in_real_time(request, subscription_id):
